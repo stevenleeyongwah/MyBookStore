f4820a868087930fde08d3f0fd0b18120acebe60
Yong Wah Steven Lee <yong.wah.steven.lee-ext@continental-corporation.com>
2020-11-22 15:15:56 +0800

    Added author route

--- server.js
+++ server.js
@@ -5,15 +5,22 @@ if ( process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+
 const mongoose = require('mongoose')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
+
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+
 app.listen(process.env.PORT || 3000)
 
 // Connect to mongodb database
